3e6065ccedfdf9822ec482ca6dfd25dcec304ceb
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-11-24 14:51:58 +0100

    Import all to main js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import * as render from "./render/index.js";
 import * as handlers from "./handlers/index.js";
+import { getProfiles } from "./api/profile/getProfiles.js";
 
 render.renderListings();
+render.renderProfile();
 
 handlers.registerFormListener();
 handlers.loginFormListener();
+handlers.updateProfileListener();
+getProfiles();
