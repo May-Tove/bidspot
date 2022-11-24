f67a04e0c03fcb473c8ddc476fb2db8a1657ca78
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-11-24 21:17:29 +0100

    Update nav status to if user is logged in or not

---
+++ src/js/templates/navbar/newListing.js
@@ -0,0 +1,9 @@
+export const newListingBtn = () => {
+  return `<a
+    role="link"
+    class="mb-2 mb-lg-0 btn btn-primary btn-sm"
+    href="/src/pages/listings/create/index.html"
+  >
+    <i class="fa-solid fa-plus"></i> New Listing</a
+  >`;
+};
