c05eeddb7f6561479ff0139f23748e70e840ec9d
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-12-08 22:03:59 +0100

    Clean up HTML semantics profile page

--- src/js/templates/profiles/profilePage.js
+++ src/js/templates/profiles/profilePage.js
@@ -19,49 +19,44 @@ export const profilePage = (profile) => {
   } else {
     editBtn = `<button
     class="btn btn-edit btn-primary rounded-circle position-absolute bottom-0 end-0"
-    data-bs-toggle="tooltip"
-    data-bs-placement="bottom"
-    data-bs-title="Edit Profile Picture"
+    type="button"
+    data-bs-toggle="modal"
+    data-bs-target="#editProfileModal"
+    id="editProfileBtn"
   >
-    <span
-      class="d-flex align-items-center justify-content-center"
-      data-bs-toggle="modal"
-      data-bs-target="#editProfileModal"
-      id="editProfileBtn"
-      ><i class="fa-regular fa-pen-to-square"></i
-    ></span>
+    <span class="d-flex align-items-center justify-content-center">
+      <i class="fa-regular fa-pen-to-square"></i>
+      </span>
   </button>`;
   }
 
-  return `<div class="w-100 bg-white rounded p-4">
-<div
-  class="d-flex flex-column align-items-center justify-content-center mb-3"
->
-  <div class="position-relative">
-    <img
-      src="${avatar}"
-      alt="Profile picture of ${profile.name}"
-      class="profile-picture rounded-circle"
-    />
-    ${editBtn}
-  </div>
-
-  <h5 class="mt-2 mb-0 fw-bold">${profile.name}</h5>
-  <p class="text-muted">${profile.email}</p>
-</div>
-<div class="d-flex flex-row flex-md-column flex-lg-row align-items-center justify-content-around p-3 bg-light rounded">
-  <div class="d-flex flex-column align-items-center">
-    <p class="m-0 small text-muted">Credit</p>
-    <span class="fw-bold number">${profile.credits}</span>
-  </div>
-  <div class="d-flex flex-column align-items-center">
-  <p class="m-0 small text-muted">Listings</p>
-  <span class="fw-bold number">${profile._count.listings}</span>
-</div>
-<div class="d-flex flex-column align-items-center">
-<p class="m-0 small text-muted">Wins</p>
-<span class="fw-bold number">${profile.wins.length}</span>
-</div>
-</div>
-</div>`;
+  return `
+  <div class="w-100 bg-white rounded p-4">
+    <div class="d-flex flex-column align-items-center justify-content-center mb-3">
+        <div class="position-relative">
+          <img
+            src="${avatar}"
+            alt="Profile picture of ${profile.name}"
+            class="profile-picture rounded-circle"
+          />
+          ${editBtn}
+        </div>
+        <h1 class="mt-2 mb-0 fw-bold fs-5">${profile.name}</h1>
+        <a href="mailto: ${profile.email}" class="text-muted">${profile.email}</a>
+      </div>
+      <div class="d-flex flex-row flex-md-column flex-lg-row align-items-center justify-content-around p-3 bg-light rounded">
+        <div class="d-flex flex-column align-items-center">
+          <p class="m-0 small text-muted">Credit</p>
+          <span class="fw-bold number">${profile.credits}</span>
+        </div>
+        <div class="d-flex flex-column align-items-center">
+          <p class="m-0 small text-muted">Listings</p>
+          <span class="fw-bold number">${profile._count.listings}</span>
+        </div>
+      <div class="d-flex flex-column align-items-center">
+        <p class="m-0 small text-muted">Wins</p>
+        <span class="fw-bold number">${profile.wins.length}</span>
+      </div>
+    </div>
+  </div>`;
 };
--- src/js/templates/profiles/wins.js
+++ src/js/templates/profiles/wins.js
@@ -1,13 +1,10 @@
 export const profileWins = (wins, index) => {
   const winNumber = index + 1;
   return `
-      <tr>
-        <th scope="row">${winNumber}</th>
-        <td><a
-        href="/src/pages/listings/details/index.html?id=${wins}"
-      >
-      ${wins}
-      </a>
+  <tr>
+    <th scope="row">${winNumber}</th>
+      <td>
+        <a href="/src/pages/listings/details/index.html?id=${wins}">${wins}</a>
       </td>
-      </tr>`;
+  </tr>`;
 };
