45ffa49a73b4c39779dfba23be1ed1bde2903689
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-12-18 19:48:25 +0100

    Makes get all listings function more reusable

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,3 +1,5 @@
 export const api_base_url = "https://nf-api.onrender.com/api/v1";
 export const api_auction_url = `${api_base_url}/auction`;
 export const api_profile_url = `${api_auction_url}/profiles`;
+export const listings_url = `${api_auction_url}/listings`;
+export const listings_flags = `sort=created&sortOrder=desc&_active=true&_bids=true&_seller=true`;
--- src/js/api/listings/getAllListings.js
+++ src/js/api/listings/getAllListings.js
@@ -1,13 +1,10 @@
-import { api_auction_url } from "../constants.js";
 import { fetchError } from "../../error/error.js";
 
-const endpoint = "/listings";
 /**
  * Get all listings from API using GET method(default)
  */
-export async function getListings() {
-  const getListingsUrl = `${api_auction_url}${endpoint}?sort=created&sortOrder=desc&_seller=true&_bids=true`;
-  const response = await fetch(getListingsUrl);
+export async function getListings(url) {
+  const response = await fetch(url);
 
   if (response.ok) {
     return await response.json();
