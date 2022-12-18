fdb31d0f1f752e4afe72956f9086435278202fe8
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-12-18 19:44:06 +0100

    Create reusable function for countdown

--- src/js/templates/error/fetchError.js
+++ src/js/templates/error/fetchError.js
@@ -1,8 +1,7 @@
 export const fetchMessage = (errorMessage) => {
   return `<div class="container d-flex align-items-center justify-content-center mt-2 py-2 rounded" id="fetchError">
-    <div class="row d-flex align-items-center justify-content-center">
-   <p class="fw-bold text-center m-0"><i class="fa-solid fa-triangle-exclamation"></i> ${errorMessage}</p>
-   </div>
-  </div>
-  `;
+            <div class="row d-flex align-items-center justify-content-center">
+              <p class="fw-bold text-center m-0"><i class="fa-solid fa-triangle-exclamation"></i> ${errorMessage}</p>
+            </div>
+          </div>`;
 };
