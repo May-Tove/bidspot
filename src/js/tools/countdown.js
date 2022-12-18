87ac3e4300c752c6bb597e4f67b38e049338f04a
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-12-18 19:44:34 +0100

    Reusable function for countdown

---
+++ src/js/tools/countdown.js
@@ -0,0 +1,21 @@
+export const countdown = (date) => {
+  const countDate = new Date(date).getTime();
+
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const second = 1000;
+  const minutes = second * 60;
+  const hour = minutes * 60;
+  const day = hour * 24;
+
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinutes = Math.floor((gap % hour) / minutes);
+
+  if (gap <= 0) {
+    return `<p class="m-0 text-danger">Auction has ended</p>`;
+  } else {
+    return `<p class="m-0"><i class="fa-regular fa-clock"></i> ${textDay}d ${textHour}h ${textMinutes}m</p>`;
+  }
+};
